29b454dfd91f2347ac9e51dcc3e524451cb57b9b
Peter Droogmans (attiks) <peter@attiks.com>
2025-06-02 12:11:55 +0200

    Change UA

--- app/app.js
+++ app/app.js
@@ -263,7 +263,7 @@ app.post('/extract', [
             try {
               // Set the user agent.
               await page.setUserAgent(
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
               );
 
               // Set duration until Timeout
